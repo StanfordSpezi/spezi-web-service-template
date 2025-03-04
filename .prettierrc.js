a347c2e9b3efe96ebe34461031205c7120e9b759
Warren Hiemstra <wahiemstra@lh.ca>
2025-03-04 14:58:22 -0500

    Remove extra license header

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,17 +4,6 @@
 //
 // SPDX-License-Identifier: MIT
 
-// This source file is part of the Stanford Spezi open-source project
-//
-// SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
-//
-// SPDX-License-Identifier: MIT
-//This source file is part of the Stanford Spezi open-source project
-//
-//SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
-//
-//SPDX-License-Identifier: MIT
-
 const { prettierConfig } = require("@stanfordspezi/spezi-web-configurations");
 
 module.exports = prettierConfig;
